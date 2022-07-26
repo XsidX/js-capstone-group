b8087ef1545aa4242b52df2447433a3bb8eabfbe
iamsid <developer.sid@outlook.com>
2022-07-27 00:38:10 +0300

    add events: open comments popup, post comment, close comments popup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
-import _ from 'lodash'; // eslint-disable-line
-import './styles.css';
+/* eslint-disable camelcase */
+import commentsPop from './modules/comments.js';
+import { postComment } from './modules/API.js';
+
+import './styles/comments.css';
+
+window.addEventListener('load', 'home');
+
+const content = document.querySelector('#content');
+
+// Event: Open comments popup
+content.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.btn-comments');
+  if (!clicked) return;
+  const animeId = +clicked.dataset.popup;
+  commentsPop(animeId);
+});
+
+// Event: Post comment
+content.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const submit = e.target.closest('.comments-form');
+  if (!submit) return;
+
+  const item_id = submit.dataset.animeid;
+  const username = document.querySelector('#name').value;
+  const comment = document.querySelector('#textarea').value;
+
+  const commentBody = {
+    item_id,
+    username,
+    comment,
+  };
+  postComment(commentBody);
+});
+
+// Event: Close comments/reservations popup
+content.addEventListener('click', (e) => {
+  const clicked = e.target.closest('.btn-close');
+  if (!clicked) return;
+
+  const commentsPopup = document.querySelector('#comments-popup');
+  commentsPopup.remove();
+});
