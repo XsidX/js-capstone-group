315d200cee16ad4823d01e8a0438e30b7c0af7c2
iamsid <developer.sid@outlook.com>
2022-07-27 18:23:01 +0300

    fix likes displayed when an item has not been liked yet

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,29 +1,22 @@
 import { getAnime, getLikes } from './API.js';
 
-const section = document.querySelector('#content');
+const section = document.querySelector('.content');
 
 const homePage = async () => {
   const data = await getAnime();
   const likesData = await getLikes();
-  let numLike = 0;
   data.forEach((anime) => {
-    likesData.find((like) => {
-      if (like.item_id === anime.mal_id) {
-        numLike = like.likes;
-      }
-      return numLike;
-    });
-    section.innerHTML += `
-        <div class="item">
-          <div class="img-container">
-          <img src='${anime.images.jpg.image_url}' alt='${anime.title} image '>
-          </div>
-          <h6>${anime.title}</h6> 
-          <h6><i class="fa fa-heart" data-likeid='${anime.mal_id}'></i>${numLike} Likes</h6>
-          <br>
-          <button type="button" class="btn btn-info btn-cm" data-cm_popup='${anime.mal_id}'>Comment</button>
-          <button type="button" class="btn btn-success">Reservation</button>
-        </div>`;
+    const likes = likesData.find((like) => like.item_id === anime.mal_id) || [];
+    section.innerHTML += `<div class="item">
+        <div class="img-container">
+         <img src='${anime.images.jpg.image_url}' alt='${anime.title} image '>
+        </div>
+        <h6>${anime.title}</h6> 
+        <h6><i class="fa fa-heart" data-likeid='${anime.mal_id}'></i> ${likes.likes || 0} Likes</h6>
+        <br>
+        <button class="btn btn-info btn-cm" data-cm_popup='${anime.mal_id}'>Comments</button>
+        <button class="btn btn-success">Reservations</button>
+      </div>`;
   });
 };
 
